3e50a4142fb51c9e214ac18b175bf66614717b31
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2024-10-30 11:35:14 -0500

    Adding color scheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import { useContext } from 'react';
+import { Context } from './context/GlobalState';
 
 function App() {
-  return <div className='bg-indigo-500'>App</div>;
+  const { theme, handleChangeTheme } = useContext(Context);
+  return (
+    <div
+      className={`min-h-screen flex flex-col bg-gray-100 text-gray-950 dark:bg-gray-950 dark:text-gray-50 ${
+        theme === 'dark' ? 'dark' : ''
+      }`}
+    >
+      App
+      <button
+        className='bg-indigo-500 text-white'
+        onClick={handleChangeTheme}
+      >
+        Theme
+      </button>
+    </div>
+  );
 }
 
 export default App;
