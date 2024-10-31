f244b04c7800197216efe611811abc46369d1565
Franklin Hurtado Salazar <137368806+Frankhs899@users.noreply.github.com>
2024-10-31 00:58:44 -0500

    Defining data in context for rendering

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -14,6 +14,26 @@ export const GlobalProvider = ({ children }) => {
     return 'light';
   });
 
+  const [transactions, setTransactions] = useState(() => {
+    const storedTransactions = localStorage.getItem('transactions');
+    return storedTransactions ? JSON.parse(storedTransactions) : [];
+  });
+
+  const [balance, setBalance] = useState(() => {
+    const storedBalance = localStorage.getItem('balance');
+    return storedBalance ? parseFloat(storedBalance) : 0;
+  });
+
+  const [income, setIncome] = useState(() => {
+    const storedIncome = localStorage.getItem('income');
+    return storedIncome ? parseFloat(storedIncome) : 0;
+  });
+
+  const [expense, setExpense] = useState(() => {
+    const storedExpense = localStorage.getItem('expense');
+    return storedExpense ? parseFloat(storedExpense) : 0;
+  });
+
   useEffect(() => {
     if (theme === 'dark') {
       document.querySelector('html').classList.add('dark');
@@ -21,6 +41,24 @@ export const GlobalProvider = ({ children }) => {
       document.querySelector('html').classList.remove('dark');
     }
   }, [theme]);
+
+  useEffect(() => {
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+  }, [transactions]);
+
+  useEffect(() => {
+    localStorage.setItem('balance', balance.toString());
+  }, [balance]);
+
+  // Guardar income en localStorage cada vez que cambie
+  useEffect(() => {
+    localStorage.setItem('income', income.toString());
+  }, [income]);
+
+  // Guardar expense en localStorage cada vez que cambie
+  useEffect(() => {
+    localStorage.setItem('expense', expense.toString());
+  }, [expense]);
 
   const handleChangeTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
@@ -33,6 +71,14 @@ export const GlobalProvider = ({ children }) => {
       value={{
         theme,
         handleChangeTheme,
+        transactions,
+        balance,
+        income,
+        expense,
+        setTransactions,
+        setBalance,
+        setIncome,
+        setExpense,
       }}
     >
       {children}
